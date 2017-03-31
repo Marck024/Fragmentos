9636803b9ac4aa157508ba0720556674153e890a
Marck024 <mpardo@upsin.edu.mx>
2017-03-30 20:18:29 -0700

    Fragmento dinamico 1.- crear fragmento 2.- agregar id contenedor en activity main 3.- modificar layout de fragmento_frag1 (para notar la diferencia) 4.- crear una variable del tipo de la clase fragmento creada 5.- preparar transacciones de fragmento

--- app/src/main/java/com/example/mp/fragmentos/MainActivity.java
+++ app/src/main/java/com/example/mp/fragmentos/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mp.fragmentos;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn=(Button)findViewById(R.id.btnprincipal);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Frag1 mifragmento=new Frag1();
+
+                FragmentTransaction fTrans=getFragmentManager().beginTransaction();
+                fTrans.replace(R.id.contenedor,mifragmento);
+                fTrans.addToBackStack(null);
+                fTrans.commit();
+
+            }
+        });
     }
 }
